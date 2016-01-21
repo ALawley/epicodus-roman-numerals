c1fb84f9b94639acf160c14fae74063015807d9e
Anna Lawley and Joshua Gustafson <alawley1@gmail.com>
2016-01-21 13:31:10 -0800

    add function for evaluating tens digit

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,44 @@
 // Example document ready function and form submit function w/ event handler to prevent default
 var romanNumeral = function(number) {
-  var str = "";
-  for (var index = 1; index <= number; index +=1) {
-    str += "I";
-    if (index === 4) {
-      str = "IV";
-    } else if (index === 5) {
-      str = "V";
-    } else {}
+  var numberArray = [];
+  for (var index = 1; index <= number.length; index +=1) {
+    numberArray.push(number.slice((index - 1), index));
   };
+  var str = "";
+
+  str= strTens + strOnes;
   return str;
 }
 
+var ones = function(number) {
+
+  var strOnes = "";
+  for (var index = 1; index <= number; index +=1) {
+    strOnes += "I";
+    if (index === 4) {
+      strOnes = "IV";
+    } else if (index === 5) {
+      strOnes = "V";
+    } else {}
+  };
+  return strOnes;
+}
+
+var tens = function(number) {
+
+  var strTens = "";
+  for (var index = 1; index <= number; index +=1) {
+    strTens += "X";
+    if (index === 4) {
+      strTens = "XL";
+    } else if (index === 5) {
+      strTens = "L";
+    } else if (index === 9) {
+      strTens = "XC";
+    }  else {}
+  };
+  return strTens;
+}
 $(document).ready(function() {
   $("form").submit(function(event){
     var str = $("input#puzzleString").val();
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,23 @@
-describe("romanNumeral", function(){
-  it("returns I as a default", function() {
-    expect(romanNumeral(1)).to.equal("I");
-  });
+// describe("romanNumeral", function(){
+//   it("returns I as a default", function() {
+//     expect(romanNumeral(1)).to.equal("I");
+//   });
+//   it("evaluates single digit numbers into roman numerals", function() {
+//     expect(romanNumeral(7)).to.equal("VII");
+//   });
+  // it("evaluates two digit numbers into roman numerals", function() {
+  //   expect(romanNumeral(73)).to.equal("LXXIII");
+  // });
+// });
+
+describe("ones", function() {
   it("evaluates single digit numbers into roman numerals", function() {
-    expect(romanNumeral(7)).to.equal("VII");
+    expect(ones(7)).to.equal("VII");
+  });
+});
+
+describe("tens", function () {
+  it("evaluate single digit fed in into tens place roman numerals", function() {
+    expect(tens(9)).to.equal("XC");
   });
 });
